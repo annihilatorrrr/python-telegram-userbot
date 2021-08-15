13c29fe7842dc80552d4b39df71fb4f4aa4aca5c
Haider <1337neo@protonmail.com>
2021-08-15 23:44:20 +0530

    rework filter handlers to use message_filters cache. Rework how filters are processed, before they only checked start of the message, now they check the entire message

--- bot/handlers/filters.py
+++ bot/handlers/filters.py
@@ -1,15 +1,12 @@
 from pyrogram import filters
 from pyrogram.handlers import MessageHandler
 from .. import utils
-from ..db import Filter
+from ..db import Filter, message_filters
 from ..config import log_group
 from logzero import logger
 
-message_filters = utils.get_message_filters()
-
 
 def handle_add_filter(client, msg):
-    global message_filters
     if len(msg.command) <= 1:
         return False
     filter_text = msg.command[1]
@@ -35,12 +32,11 @@ def handle_add_filter(client, msg):
                reply_text=reply_text,
                message_id=message_id)
     f.save()
+    message_filters.add(filter_text, f, replace=True)
     msg.edit_text(f'Filter saved for {filter_text}')
-    message_filters = utils.get_message_filters()
 
 
 def handle_remove_filter(client, msg):
-    global message_filters
     if len(msg.command) <= 1:
         return False
     filter_text = msg.command[1]
@@ -51,29 +47,36 @@ def handle_remove_filter(client, msg):
         return False
 
     q = q[0].delete_instance()
+    message_filters.remove(filter_text)
     msg.edit_text(f'Filter removed for {filter_text}')
-    message_filters = utils.get_message_filters()
 
 
 def process_filter(client, msg):
     msg_text = msg.text or msg.caption
     if msg_text:
+        filter_keys = set(message_filters.allkeys())
+        text_words = set(msg_text.split(' '))
+        intersection = filter_keys.intersection(text_words)
+        if not len(intersection):
+            return False
+        matches = list(intersection)
+        filter_text = matches[0]
         try:
-            filter_ = message_filters[msg_text]
-            if 'message_id' in filter_:
-                client.copy_message(
-                    from_chat_id=log_group,
-                    chat_id=msg.chat.id,
-                    message_id=filter_['message_id'],
-                    reply_to_message_id=msg.message_id
-                )
-            elif 'reply_text' in filter_:
-                try:
-                    msg.reply_text(filter_['reply_text'])
-                except Exception as e:
-                    logger.error(str(e))
-        except KeyError:
-            pass
+            filterr = message_filters.get(filter_text)
+        except Exception:
+            return False
+
+        if filterr.message_id:
+            # copy message
+            client.copy_message(
+                from_chat_id=log_group,
+                chat_id=msg.chat.id,
+                message_id=filterr.message_id,
+                reply_to_message_id=msg.message_id
+            )
+        else:
+            msg.reply_text(filterr.reply_text)
+
     msg.continue_propagation()
 
 
