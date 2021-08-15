2ca5b9d7bc00285b1879532015b31915518d1976
Haider <1337neo@protonmail.com>
2021-08-15 20:27:46 +0530

    pydantic models for filters and users

--- bot/schema.py
+++ bot/schema.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional, List
 
 
@@ -6,3 +6,13 @@ class FilterSchema(BaseModel):
     message_id: Optional[int]
     filter_text: str
     reply_text: Optional[str]
+
+
+class UserSchema(BaseModel):
+    user_id: int = Field(source='id')
+    warns: int = Field(source='warns')
+    allowed: int = Field(source='blocked')
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
