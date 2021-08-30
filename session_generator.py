2fe1c452e43e328c564d77177436742ac7b8bae8
Haider <me@haiderali.dev>
2021-08-30 20:47:36 +0530

    generate session file for bot and user

--- session_generator.py
+++ session_generator.py
@@ -1,10 +1,25 @@
 from pyrogram import Client
 from dotenv import load_dotenv
 import os
+from logzero import logger
+import asyncio
+
 
 load_dotenv()
+phone_number = os.getenv('phone_number')
 api_id = os.getenv('api_id')
 api_hash = os.getenv('api_hash')
+bot_token = os.getenv('bot_token')
 
-with Client(":memory", api_id, api_hash) as app:
-    print(app.export_session_string())
+
+async def work():
+    logger.info('Attempting to login to account')
+    client = Client('user', api_id, api_hash, phone_number=phone_number)
+    await client.start()
+    bot_client = Client('bot', api_id, api_hash, bot_token=bot_token)
+    await bot_client.start()
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(work())
